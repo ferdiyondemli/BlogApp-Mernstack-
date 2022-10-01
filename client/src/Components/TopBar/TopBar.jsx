a47bc6390c0b76957d5ad951f7f1a001bc4beb2e
ferdi yöndemli <ferdiyondemli50@gmail.com>
2022-10-01 20:16:28 +0300

    responsive done

--- client/src/Components/TopBar/TopBar.jsx
+++ client/src/Components/TopBar/TopBar.jsx
@@ -44,11 +44,11 @@ dispatch({
         </ul>
       </div>
       <div className="topright">
-        {user?( <img
+        {user?(<Link to={"/settings"}><img
           className="topImg"
           src={user.profilePic||"https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"}
           alt="Your"
-        />):(<>
+        /></Link> ):(<>
         <Link className="Link topListItem" to="/register">REGISTER</Link>
         <Link className="Link topListItem" to="/login">LOGIN</Link>
         </>)}
